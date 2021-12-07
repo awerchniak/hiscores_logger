30aba37fcccd9c390282c9072fa8ed6131d508c2
Andrew Werchniak <andy.werchniak@gmail.com>
2021-12-07 00:49:20 -0500

    Write to DDB in GetAndParseLambda

--- hiscores_logger/hiscores_logger_stack.py
+++ hiscores_logger/hiscores_logger_stack.py
@@ -1,5 +1,7 @@
 from aws_cdk import (
+    aws_dynamodb as ddb,
     aws_lambda as _lambda,
+    RemovalPolicy,
     Stack,
 )
 from constructs import Construct
@@ -14,16 +16,30 @@ class HiscoresLoggerStack(Stack):
 
         super().__init__(scope, construct_id, **kwargs)
 
+        table = ddb.Table(
+            self,
+            "HiScores",
+            partition_key=ddb.Attribute(name="player", type=ddb.AttributeType.STRING),
+            sort_key=ddb.Attribute(name="timestamp", type=ddb.AttributeType.STRING),
+            encryption=ddb.TableEncryption.AWS_MANAGED,
+            read_capacity=5,
+            write_capacity=5,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
         code_dir = "lambda"
         function_name = "GetAndParseOSRSMetricsLambda"
 
         with TemporaryDirectory() as layer_output_dir:
-            _lambda.Function(
+            handler = _lambda.Function(
                 self,
                 function_name,
                 runtime=_lambda.Runtime.PYTHON_3_8,
                 code=_lambda.Code.from_asset(code_dir),
                 handler="get_and_parse_metrics.handler",
+                environment={
+                    "HISCORES_TABLE_NAME": table.table_name,
+                },
                 layers=[
                     self.create_dependencies_layer(
                         layer_id=f"{self.stack_name}-{function_name}-dependencies",
@@ -32,6 +48,8 @@ class HiscoresLoggerStack(Stack):
                     )
                 ],
             )
+
+        table.grant_read_write_data(handler)
 
     def create_dependencies_layer(
         self, layer_id: str, requirements_file: str, output_dir: str
--- lambda/get_and_parse_metrics.py
+++ lambda/get_and_parse_metrics.py
@@ -1,18 +1,33 @@
+import boto3
 import json
 import logging
+import os
 
 import rs_api
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
+ddb = boto3.resource("dynamodb")
+table = ddb.Table(os.environ["HISCORES_TABLE_NAME"])
+
 
 def handler(event, context):
     """Call HiScores API and parse response."""
 
     player = event["player"]
+
+    # retrieve HiScores for `player`
     logger.info(f"Getting hiscores for {player}")
     payload = rs_api.process_hiscores_response(
         rs_api.request_hiscores(player=player, timeout=45.0)
     )
+
+    # write result to `table`
+    logger.info(
+        f"Putting payload for player '{payload['player']}', timestamp '{payload['timestamp']}'"
+    )
+    logger.debug(f"Putting payload {payload}")
+    table.put_item(Item=payload)
+
     return json.dumps(payload)
