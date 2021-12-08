3ad4fab56476945e3cd6df6a9ba0c13dd8e9fa33
Andrew Werchniak <andy.werchniak@gmail.com>
2021-12-08 14:34:57 -0500

    add some validation to QueryLambda

--- lambda/read_hiscores_table/read_hiscores_table.py
+++ lambda/read_hiscores_table/read_hiscores_table.py
@@ -8,7 +8,15 @@ table = ddb.Table(os.environ["HISCORES_TABLE_NAME"])
 
 def handler(event, context):
     """Query table and return result."""
+    if "player" not in event:
+        raise ValueError("Invocation must include 'player' attribute.")
     player = event["player"]
+
+    if "timeRange" not in event or len(event["timeRange"]) != 2:
+        raise ValueError(
+            "Invocation must include 'timeRange' attribute "
+            'with shape ["YYYY-mm-dd HH:MM:SS","YYYY-mm-dd HH:MM:SS"].'
+        )
     time_range = event["timeRange"]
 
     response = table.query(
