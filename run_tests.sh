685710e4ade2ce613c3818984b289b6460434bd5
Andrew Werchniak <andy.werchniak@gmail.com>
2021-12-17 17:57:54 -0500

    Create rudimentary pytest.ini
    ref: #3

--- run_tests.sh
+++ run_tests.sh
@@ -4,24 +4,15 @@ set -exo pipefail
 NO_TESTS_FOUND=5
 
 >&2 echo "Running unit tests for constructs..."
-pytest tests \
-    -v \
-    --cov=hiscores_tracker \
-    --cov-report=term-missing \
-    --cov-fail-under=0 \
-    --cov-report=html:tests/.coverage
+pytest tests --cov=hiscores_tracker --cov-report=html:tests/.coverage
 
 >&2 echo "Running unit tests for lambda handler libs..."
 for dir in $(ls -d lambda/*)
 do
 >&2 echo "Testing $dir..."
 pytest $dir \
-    -v \
-    --doctest-modules \
     --ignore=$dir/handler.py \
     --cov=$dir \
-    --cov-report=term-missing \
-    --cov-fail-under=0 \
     --cov-report=html:$dir/.coverage \
     || NO_TESTS_FOUND=$?
 done
