48398119ee1dd93604bf1f748dfe714196269132
Andrew Werchniak <andy.werchniak@gmail.com>
2021-12-17 14:09:28 -0500

    Enable pytest coverage checks
    ref: #3

--- run_tests.sh
+++ run_tests.sh
@@ -3,13 +3,14 @@ set -exo pipefail
 
 NO_TESTS_FOUND=5
 
->&2 echo "Running construct unit tests..."
-pytest tests
+>&2 echo "Running unit tests for constructs..."
+pytest tests --cov=hiscores_tracker --cov-report=term-missing --cov-fail-under=0
 
+>&2 echo "Running unit tests for lambda handler libs..."
 for dir in $(ls -d lambda/*)
 do
->&2 echo "Running tests for $dir lambda..."
-pytest $dir --doctest-modules --ignore-glob=*handler.py || NO_TESTS_FOUND=$?
+>&2 echo "Testing $dir..."
+pytest $dir --doctest-modules --ignore-glob=*handler.py --cov=$dir --cov-report=term-missing --cov-fail-under=0 || NO_TESTS_FOUND=$?
 done
 
 >&2 echo "Cleaning pycaches..."
