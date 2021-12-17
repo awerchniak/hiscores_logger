b585a3457d2786f1a396dcd37ba137f6e4a9b651
Andrew Werchniak <andy.werchniak@gmail.com>
2021-12-17 17:09:59 -0500

    Output coverage HTML with tests
    ref: #3

--- run_tests.sh
+++ run_tests.sh
@@ -4,13 +4,26 @@ set -exo pipefail
 NO_TESTS_FOUND=5
 
 >&2 echo "Running unit tests for constructs..."
-pytest tests --cov=hiscores_tracker --cov-report=term-missing --cov-fail-under=0
+pytest tests \
+    -v \
+    --cov=hiscores_tracker \
+    --cov-report=term-missing \
+    --cov-fail-under=0 \
+    --cov-report=html:tests/.coverage
 
 >&2 echo "Running unit tests for lambda handler libs..."
 for dir in $(ls -d lambda/*)
 do
 >&2 echo "Testing $dir..."
-pytest $dir --doctest-modules --ignore-glob=*handler.py --cov=$dir --cov-report=term-missing --cov-fail-under=0 || NO_TESTS_FOUND=$?
+pytest $dir \
+    -v \
+    --doctest-modules \
+    --ignore=$dir/handler.py \
+    --cov=$dir \
+    --cov-report=term-missing \
+    --cov-fail-under=0 \
+    --cov-report=html:$dir/.coverage \
+    || NO_TESTS_FOUND=$?
 done
 
 >&2 echo "Cleaning pycaches..."
